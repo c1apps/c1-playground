7a81fb0d3008e66f0a3527d8ee609be50f3a0fcd
Iñaki Zárate <64897117+c1apps@users.noreply.github.com>
2023-02-08 22:20:31 +0100

    Deploy jenkins on EKS

---
+++ bin/deploy-jenkins-eks.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#######################################
+# Main:
+# Deploys Jenkins on EKS Cluster
+#######################################
+
+function create_namespace() {
+  # create namespace
+  printf '%s' "Create jenkins namespace"
+  kubectl create namespace jenkins
+  printf '%s\n' " 🍼"
+}
+
+function whitelist_namespaces() {
+  # whitelist some namespace for container security
+  kubectl label namespace jenkins --overwrite ignoreAdmissionControl=true
+}
+
+function create_ecr_repository() {
+  # Exports
+  export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+  export AWS_REGION=$(aws configure get region)
+  export AWS_ECR=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+  # Create ECR repository
+  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
+  if [[ $(aws ecr describe-repositories) =~ :repository/c1-jenkins ]]; then
+    echo "c1-jenkins repository already exists"
+  else
+  aws ecr create-repository \
+    --repository-name c1-jenkins \
+    --image-scanning-configuration scanOnPush=true \
+    --region $AWS_REGION
+  fi
+}
+
+function create_jenkins_image() {
+  printf '%s\n' "Create jenkins image"
+
+  docker build -t $AWS_ECR/c1-jenkins:latest - < ${PGPATH}/templates/jenkins-eks-dockerfile
+
+  printf '%s\n' "Jenkins image created 🍻"
+
+  docker push $AWS_ECR/c1-jenkins:latest
+
+  printf '%s\n' "Jenkins image pushed to ECR 🍻"
+}
+
+function main() {
+  create_namespace
+  whitelist_namespaces
+  create_ecr_repository
+  create_jenkins_image
+}
+
+
+# run main of no arguments given
+if [[ $# -eq 0 ]] ; then
+  main
+fi
