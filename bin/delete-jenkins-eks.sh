4287dd15e57e7b77358406b32b069e76f2ecc8f2
Iñaki Zárate <64897117+c1apps@users.noreply.github.com>
2023-02-09 16:03:12 +0100

    Delete Jenkins on EKS

--- bin/delete-jenkins-eks.sh
+++ bin/delete-jenkins-eks.sh
@@ -5,11 +5,15 @@ export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output t
 export AWS_REGION=$(aws configure get region)
 export AWS_ECR=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
-function delete_namespace() {
-  # create namespace
-  printf '%s' "Create jenkins namespace"
-  kubectl create namespace jenkins
-  printf '%s\n' " 🍼"
+function clean_ecr_repository() {
+  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
+  if [[ $(aws ecr describe-repositories) =~ :repository/c1-jenkins ]]; then
+    aws ecr batch-delete-image --region $AWS_REGION
+    --image-ids "$(aws ecr list-images --region $AWS_REGION --repository-name c1-jenkins --query 'imageIds[*]' --output json
+)" || true
+  else
+    echo "c1-jenkins repository already deleted"
+  fi
 }
 
 function delete_ecr_repository() {
@@ -23,8 +27,13 @@ function delete_ecr_repository() {
   fi
 }
 
+aws ecr batch-delete-image \
+     --repository-name my-repo \
+     --image-ids imageTag=tag1 imageTag=tag2
+
 kubectl delete service c1-jenkins -n jenkins
 kubectl delete deploy c1-jenkins -n jenkins
 kubectl delete pvc jenkins-pvc -n jenkins
-delete_namespace
+kubectl delete namespace jenkins
+clean_ecr_repository
 delete_ecr_repository
