22b91d822705f10b9375ba8d65e72b91911b2fcc
Iñaki Zárate <64897117+c1apps@users.noreply.github.com>
2023-02-09 16:08:08 +0100

    Delete Jenkins on EKS

--- bin/delete-jenkins-eks.sh
+++ bin/delete-jenkins-eks.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Exports
-export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
-export AWS_REGION=$(aws configure get region)
-export AWS_ECR=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 function clean_ecr_repository() {
-  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
+  export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
+  export AWS_REGION=$(aws configure get region)
+  export AWS_ECR=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
   if [[ $(aws ecr describe-repositories) =~ :repository/c1-jenkins ]]; then
     aws ecr batch-delete-image --region $AWS_REGION
     --image-ids "$(aws ecr list-images --region $AWS_REGION --repository-name c1-jenkins --query 'imageIds[*]' --output json
@@ -17,7 +16,7 @@ function clean_ecr_repository() {
 }
 
 function delete_ecr_repository() {
-  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ECR
+  export AWS_REGION=$(aws configure get region)
   if [[ $(aws ecr describe-repositories) =~ :repository/c1-jenkins ]]; then
     aws ecr delete-repository \
     --repository-name c1-jenkins \
@@ -26,10 +25,6 @@ function delete_ecr_repository() {
     echo "c1-jenkins repository already deleted"
   fi
 }
-
-aws ecr batch-delete-image \
-     --repository-name my-repo \
-     --image-ids imageTag=tag1 imageTag=tag2
 
 kubectl delete service c1-jenkins -n jenkins
 kubectl delete deploy c1-jenkins -n jenkins
