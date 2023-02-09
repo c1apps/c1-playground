5f3c9b7670a8b605503a4b84444efd14ae2643db
Iñaki Zárate <64897117+c1apps@users.noreply.github.com>
2023-02-09 16:22:27 +0100

    Delete Jenkins from EKS

--- bin/delete-jenkins-eks.sh
+++ bin/delete-jenkins-eks.sh
@@ -7,10 +7,10 @@ function clean_ecr_repository() {
   export AWS_REGION=$(aws configure get region)
   export AWS_ECR=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
   if [[ $(aws ecr describe-repositories) =~ :repository/c1-jenkins ]]; then
-    aws ecr batch-delete-image --region $AWS_REGION \
+    aws ecr batch-delete-image --region $AWS_REGION --repository-name c1-jenkins\
     --image-ids "$(aws ecr list-images --region $AWS_REGION --repository-name c1-jenkins --query 'imageIds[*]' --output json)" || true
   else
-    echo "c1-jenkins repository already deleted"
+    echo "no images to delete in repo"
   fi
 }
 
