4b2472ea9fa2de84db8c786e70097a7f1a061c20
c1apps <izapps@pm.me>
2023-03-02 20:21:53 +0000

    updated

--- bin/delete-jenkins-eks.sh
+++ bin/delete-jenkins-eks.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Exports
-
 function clean_ecr_repository() {
   export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --output text)
   export AWS_REGION=$(aws configure get region)
